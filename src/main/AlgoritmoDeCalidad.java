0be727e93d14601cc2f811f4d3a4c9a904d39461
Thomas <thomi.reynoso@hotmail.com>
2018-05-08 11:49:04 -0300

    Se añade Ventana de Funcionabilidad y Eficiencia

--- src/main/AlgoritmoDeCalidad.java
+++ src/main/AlgoritmoDeCalidad.java
@@ -2,33 +2,77 @@ package main;
 
 import javax.swing.JOptionPane;
 
+import com.sun.xml.internal.bind.v2.runtime.reflect.Accessor.GetterSetterReflection;
+
 public class AlgoritmoDeCalidad {
 	
+	private static  Integer pesoDeFuncionabilidad,
+							pesoDeEficiencia,
+							pesoDeFiabilidad,
+							pesoDeMantenibilidad,
+							pesoDeUsabilidad,
+							pesoDePortabilidad;
+	private Integer seguridadDeAcceso,
+					exactitudDeResultados,
+					utilizacionDeRecursos,
+					comportamientoEnElTiempo,
+					toleranciaAFallos,
+					capacidadDeRecuperacionDeErrores,
+					analizabilidadDelCodigo,
+					cambiabilidadDelCodigo,
+					estabilidadDelCodigo,
+					capacidadDeSerEntendido,
+					capacidadDeSerOperado,
+					capacidadDeSerAtractivo,
+					adaptabilidad,
+					instalabilidad;
+					
+
+
+	
+	
+	public void mostrarFuncionabilidad() {
+		JOptionPane.showMessageDialog(null, "Seguridad de Acceso: (" + getSeguridadDeAcceso() + ")\n" +
+											"Exactitud de resultados: (" + getExactitudDeResultados() + ")");
+	}
+
+	public void mostrarEficiencia() {
+		JOptionPane.showMessageDialog(null, "Utilizacion de Recursos: (" + getUtilizacionDeRecursos() + ")\n" +
+				"Comportamiento En El Tiempo: (" + getComportamientoEnElTiempo() + ")");
+	}
+	
+	
+	
+	
+
+
 	public static String ingresoDeDatosYCalculoDeExperiencia()
 	{
-		Integer pesoDeFuncionabilidad = 10; // Los pesos varían de 0 a 1.
+		pesoDeFuncionabilidad = 10;
+		
+		//Integer pesoDeFuncionabilidad = 10; // Los pesos varían de 0 a 1.
 		Integer seguridadDeAcceso = Integer.parseInt(JOptionPane.showInputDialog("FUNCIONABILIDAD - Seguridad de acceso - Capacidad del producto software para asegurar la integridad de los datos y la confidencialidad de éstos - Características a medir: Encriptación de datos e inicio de sesión de usuarios - Ingrese calificación del 0 al 10."));		
 		Integer exactitudDeResultados = Integer.parseInt(JOptionPane.showInputDialog("FUNCIONABILIDAD - Exactitud de resultados - Capacidad del producto software para proporcionar los resultados con el grado necesario de precisión. - Ingrese calificación del 0 al 10."));
 		
-		Integer  pesoDeEficiencia = 5;
+		pesoDeEficiencia = 5;
 		Integer utilizacionDeRecursos = Integer.parseInt(JOptionPane.showInputDialog("EFICIENCIA - Utilización de recursos\n\nEficiencia del producto software de acuerdo al porcentaje de uso de procesador que realice. - Ingrese calificación del 0 al 10."));
 		Integer comportamientoEnElTiempo = Integer.parseInt(JOptionPane.showInputDialog("EFICIENCIA - Comportamiento en el tiempo\n\nTiempo que está el producto software sin informarle al usuario del estado en que se encuentra la solicitud que realizó. - Ingrese calificación del 0 al 10."));
 		
-		Integer pesoDeFiabilidad = 1;
+		pesoDeFiabilidad = 1;
 		Integer toleranciaAFallos = Integer.parseInt(JOptionPane.showInputDialog("FIABILIDAD - Tolerancia a fallos\n\nCapacidad del producto software para mantener la integridad de los datos cuando se producen fallas en el sistema.\n\n\nCaracterísticas a medir: Protección de los datos necesarios al generarse un error y generación de log con las actividades que el sistema estaba haciendo antes de fallar. - Ingrese calificación del 0 al 10."));
 		Integer capacidadDeRecuperacionDeErrores = Integer.parseInt(JOptionPane.showInputDialog("FIABILIDAD - Capacidad de recuperación de errores\n\nCapacidad del sistema de reanudar sus actividades cuando se producen errores críticos.\n\n\nCaracterísticas a medir: Reanudación de actividades al producirse una falla crítica y reanudación del sistema desde el estado en que estaba antes de fallar. - Ingrese calificación del 0 al 10."));
 		
-		Integer pesoDeMantenibilidad = 3;		
+		pesoDeMantenibilidad = 3;		
 		Integer analizabilidadDelCodigo = Integer.parseInt(JOptionPane.showInputDialog("MANTENIBILIDAD - Capacidad del código para ser analizado\n\nAnalizabilidad del código teniendo en cuenta el porcentaje de comentarios por método y en general. - Ingrese calificación del 0 al 10."));
 		Integer cambiabilidadDelCodigo = Integer.parseInt(JOptionPane.showInputDialog("MANTENIBILIDAD - Capacidad del código para ser cambiado\n\nAnalizabilidad del código teniendo en cuenta la complejidad ciclomática del método. - Ingrese calificación del 0 al 10."));
 		Integer estabilidadDelCodigo = Integer.parseInt(JOptionPane.showInputDialog("MANTENIBILIDAD - Estabilidad\n\nPromedio de fallas del sistema por prueba. - Ingrese calificación del 0 al 10."));
 		
-		Integer pesoDeUsabilidad = 7;
+		pesoDeUsabilidad = 7;
 		Integer capacidadDeSerEntendido = Integer.parseInt(JOptionPane.showInputDialog("USABILIDAD - Capacidad de ser entendido\n\n Capacidad del producto software para ayudar a los usuarios cuando necesiten asistencia.\n\n\nCaracterísticas a medir: Posee ayuda contextual sobre menúes y botones de acción e incorporación de manual de usuario en el sistema con un menú dedicado. - Ingrese calificación del 0 al 10."));
 		Integer capacidadDeSerOperado = Integer.parseInt(JOptionPane.showInputDialog("USABILIDAD - Capacidad de ser entendido\n\n Capacidad del producto software para ayudar a los usuarios cuando necesiten asistencia.\n\n\nManual de usuario incorporado al sistema como un menú dedicado.\nMALA - REGULAR - BUENA - Ingrese calificación del 0 al 10."));
 		Integer capacidadDeSerAtractivo = Integer.parseInt(JOptionPane.showInputDialog("USABILIDAD - Capacidad de ser operado\n\nCapacidad del producto software de ser utilizado sin asistencia adicional. - Ingrese calificación del 0 al 10."));
 		
-		Integer pesoDePortabilidad = 3;
+		pesoDePortabilidad = 3;
 		Integer adaptabilidad = Integer.parseInt(JOptionPane.showInputDialog("PORTABILIDAD - Adaptabilidad\n\nCapacidad del producto software de adaptarse a diferentes sistemas operativos sin cambiar su estructura interna. - Ingrese calificación del 0 al 10."));
 		Integer instalabilidad = Integer.parseInt(JOptionPane.showInputDialog("PORTABILIDAD - Instalabilidad\n\nCapacidad del producto software de poder ser instalado en una cantidad mínima de pasos. - Ingrese calificación del 0 al 10."));
 		
@@ -52,5 +96,149 @@ public class AlgoritmoDeCalidad {
 		}
 		return "MALA";
 	}
+
+
+	//Getters and setters
+		public Integer getSeguridadDeAcceso() {
+			return seguridadDeAcceso;
+		}
+
+
+		public void setSeguridadDeAcceso(Integer seguridadDeAcceso) {
+			this.seguridadDeAcceso = seguridadDeAcceso;
+		}
+
+
+		public Integer getExactitudDeResultados() {
+			return exactitudDeResultados;
+		}
+
+
+		public void setExactitudDeResultados(Integer exactitudDeResultados) {
+			this.exactitudDeResultados = exactitudDeResultados;
+		}
+
+
+		public Integer getUtilizacionDeRecursos() {
+			return utilizacionDeRecursos;
+		}
+
+
+		public void setUtilizacionDeRecursos(Integer utilizacionDeRecursos) {
+			this.utilizacionDeRecursos = utilizacionDeRecursos;
+		}
+
+
+		public Integer getComportamientoEnElTiempo() {
+			return comportamientoEnElTiempo;
+		}
+
+
+		public void setComportamientoEnElTiempo(Integer comportamientoEnElTiempo) {
+			this.comportamientoEnElTiempo = comportamientoEnElTiempo;
+		}
+
+
+		public Integer getToleranciaAFallos() {
+			return toleranciaAFallos;
+		}
+
+
+		public void setToleranciaAFallos(Integer toleranciaAFallos) {
+			this.toleranciaAFallos = toleranciaAFallos;
+		}
+
+
+		public Integer getCapacidadDeRecuperacionDeErrores() {
+			return capacidadDeRecuperacionDeErrores;
+		}
+
+
+		public void setCapacidadDeRecuperacionDeErrores(Integer capacidadDeRecuperacionDeErrores) {
+			this.capacidadDeRecuperacionDeErrores = capacidadDeRecuperacionDeErrores;
+		}
+
+
+		public Integer getAnalizabilidadDelCodigo() {
+			return analizabilidadDelCodigo;
+		}
+
+
+		public void setAnalizabilidadDelCodigo(Integer analizabilidadDelCodigo) {
+			this.analizabilidadDelCodigo = analizabilidadDelCodigo;
+		}
+
+
+		public Integer getCambiabilidadDelCodigo() {
+			return cambiabilidadDelCodigo;
+		}
+
+
+		public void setCambiabilidadDelCodigo(Integer cambiabilidadDelCodigo) {
+			this.cambiabilidadDelCodigo = cambiabilidadDelCodigo;
+		}
+
+
+		public Integer getEstabilidadDelCodigo() {
+			return estabilidadDelCodigo;
+		}
+
+
+		public void setEstabilidadDelCodigo(Integer estabilidadDelCodigo) {
+			this.estabilidadDelCodigo = estabilidadDelCodigo;
+		}
+
+
+		public Integer getCapacidadDeSerEntendido() {
+			return capacidadDeSerEntendido;
+		}
+
+
+		public void setCapacidadDeSerEntendido(Integer capacidadDeSerEntendido) {
+			this.capacidadDeSerEntendido = capacidadDeSerEntendido;
+		}
+
+
+		public Integer getCapacidadDeSerOperado() {
+			return capacidadDeSerOperado;
+		}
+
+
+		public void setCapacidadDeSerOperado(Integer capacidadDeSerOperado) {
+			this.capacidadDeSerOperado = capacidadDeSerOperado;
+		}
+
+
+		public Integer getCapacidadDeSerAtractivo() {
+			return capacidadDeSerAtractivo;
+		}
+
+
+		public void setCapacidadDeSerAtractivo(Integer capacidadDeSerAtractivo) {
+			this.capacidadDeSerAtractivo = capacidadDeSerAtractivo;
+		}
+
+
+		public Integer getAdaptabilidad() {
+			return adaptabilidad;
+		}
+
+
+		public void setAdaptabilidad(Integer adaptabilidad) {
+			this.adaptabilidad = adaptabilidad;
+		}
+
+
+		public Integer getInstalabilidad() {
+			return instalabilidad;
+		}
+
+
+		public void setInstalabilidad(Integer instalabilidad) {
+			this.instalabilidad = instalabilidad;
+		}
+
+	
+
 
 }
---
+++ src/swing/Funcionabilidad.java
@@ -0,0 +1,235 @@
+package swing;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import main.AlgoritmoDeCalidad;
+
+import javax.swing.JList;
+import javax.swing.JTextPane;
+import javax.swing.JFormattedTextField;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JRadioButton;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.ButtonGroup;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class Funcionabilidad extends JFrame {
+
+	private JPanel contentPane;
+	private final ButtonGroup segAcceso = new ButtonGroup();
+	private final ButtonGroup exactitudResultado = new ButtonGroup();
+	AlgoritmoDeCalidad algoritmo = new AlgoritmoDeCalidad();
+	private static Integer EXCELENTE = 10,
+						   MUY_BUENO = 8,
+						   BUENO = 6,
+						   REGULAR = 4,
+						   MALO = 2;
+						   
+						   
+	
+	
+
+
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Funcionabilidad frame = new Funcionabilidad();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Funcionabilidad() {
+		setTitle("Funcionabilidad");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 608, 526);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		establecerDefault(); //Establezco valores default (Excelente)
+		
+		JTextPane txtpnSeguridadDeAcceso = new JTextPane();
+		txtpnSeguridadDeAcceso.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		txtpnSeguridadDeAcceso.setText("Capacidad del producto software para asegurar la integridad de los datos y la confidencialidad de \u00E9stos - Caracter\u00EDsticas a medir: Encriptaci\u00F3n de datos e inicio de sesi\u00F3n de usuarios");
+		txtpnSeguridadDeAcceso.setBounds(10, 89, 306, 116);
+		contentPane.add(txtpnSeguridadDeAcceso);
+		
+		JLabel lblSeguridadDeAcceso = new JLabel("Seguridad De Acceso:");
+		lblSeguridadDeAcceso.setFont(new Font("Tahoma", Font.BOLD, 13));
+		lblSeguridadDeAcceso.setBounds(10, 45, 160, 31);
+		contentPane.add(lblSeguridadDeAcceso);
+		
+		JRadioButton rdbtnExcelenteSA = new JRadioButton("Excelente");
+		rdbtnExcelenteSA.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				algoritmo.setSeguridadDeAcceso(EXCELENTE);
+			}
+		});
+		rdbtnExcelenteSA.setSelected(true);
+		segAcceso.add(rdbtnExcelenteSA);
+		rdbtnExcelenteSA.setBounds(359, 65, 87, 23);
+		contentPane.add(rdbtnExcelenteSA);
+		
+		
+		JRadioButton rdbtnMuyBuenoSA = new JRadioButton("Muy Bueno");
+		rdbtnMuyBuenoSA.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				algoritmo.setSeguridadDeAcceso(MUY_BUENO);
+			}
+		});
+		segAcceso.add(rdbtnMuyBuenoSA);
+		rdbtnMuyBuenoSA.setBounds(359, 91, 102, 23);
+		contentPane.add(rdbtnMuyBuenoSA);
+		
+		JRadioButton rdbtnBuenoSA = new JRadioButton("Bueno");
+		rdbtnBuenoSA.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				algoritmo.setSeguridadDeAcceso(BUENO);
+			}
+		});
+		segAcceso.add(rdbtnBuenoSA);
+		rdbtnBuenoSA.setBounds(359, 117, 87, 23);
+		contentPane.add(rdbtnBuenoSA);
+		
+		
+		JRadioButton rdbtnRegularSA = new JRadioButton("Regular");
+		rdbtnRegularSA.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				algoritmo.setSeguridadDeAcceso(REGULAR);
+			}
+		});
+		segAcceso.add(rdbtnRegularSA);
+		rdbtnRegularSA.setBounds(359, 143, 87, 23);
+		contentPane.add(rdbtnRegularSA);
+		
+		
+		JRadioButton rdbtnMaloSA = new JRadioButton("Malo");
+		rdbtnMaloSA.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				algoritmo.setSeguridadDeAcceso(MALO);
+			}
+		});
+		segAcceso.add(rdbtnMaloSA);
+		rdbtnMaloSA.setBounds(359, 171, 87, 23);
+		contentPane.add(rdbtnMaloSA);
+		
+		JButton btnSiguiente = new JButton("Siguiente");
+		btnSiguiente.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				algoritmo.mostrarFuncionabilidad();
+				dispose();
+				Eficiencia ef = new Eficiencia();
+				ef.nuevaVista();
+				
+			}
+		});
+		btnSiguiente.setBounds(361, 442, 139, 23);
+		contentPane.add(btnSiguiente);
+		
+		JButton btnSalir = new JButton("Salir");
+		btnSalir.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+		btnSalir.setBounds(56, 442, 138, 23);
+		contentPane.add(btnSalir);
+	
+		JTextPane txtpnCapacidadDelProducto = new JTextPane();
+		txtpnCapacidadDelProducto.setText("Capacidad del producto software para proporcionar los resultados con el grado necesario de precisi\u00F3n.");
+		txtpnCapacidadDelProducto.setBounds(10, 294, 306, 92);
+		contentPane.add(txtpnCapacidadDelProducto);
+		
+		JLabel lblExactitudDeResultado = new JLabel("Exactitud De Resultado:");
+		lblExactitudDeResultado.setFont(new Font("Tahoma", Font.BOLD, 13));
+		lblExactitudDeResultado.setBounds(10, 258, 160, 23);
+		contentPane.add(lblExactitudDeResultado);
+		
+		JRadioButton rdbtnExcelenteER = new JRadioButton("Excelente");
+		rdbtnExcelenteER.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				algoritmo.setExactitudDeResultados(EXCELENTE);
+			}
+		});
+
+		exactitudResultado.add(rdbtnExcelenteER);
+		rdbtnExcelenteER.setSelected(true);
+		rdbtnExcelenteER.setBounds(359, 258, 87, 23);
+		contentPane.add(rdbtnExcelenteER);
+		
+		
+		JRadioButton rdbtnMuyBuenoER = new JRadioButton("Muy Bueno");
+		rdbtnMuyBuenoER.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				algoritmo.setExactitudDeResultados(MUY_BUENO);
+			}
+		});
+		exactitudResultado.add(rdbtnMuyBuenoER);
+		rdbtnMuyBuenoER.setBounds(359, 284, 102, 23);
+		contentPane.add(rdbtnMuyBuenoER);
+		
+		
+		JRadioButton rdbtnBuenoER = new JRadioButton("Bueno");
+		rdbtnBuenoER.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				algoritmo.setExactitudDeResultados(BUENO);
+			}
+		});
+		exactitudResultado.add(rdbtnBuenoER);
+		rdbtnBuenoER.setBounds(359, 310, 87, 23);
+		contentPane.add(rdbtnBuenoER);
+		
+		JRadioButton rdbtnRegularER = new JRadioButton("Regular");
+		rdbtnRegularER.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				algoritmo.setExactitudDeResultados(REGULAR);
+			}
+		});
+		exactitudResultado.add(rdbtnRegularER);
+		rdbtnRegularER.setBounds(359, 336, 87, 23);
+		contentPane.add(rdbtnRegularER);
+		
+		
+		JRadioButton rdbtnMaloER = new JRadioButton("Malo");
+		rdbtnMaloER.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				algoritmo.setExactitudDeResultados(MALO);
+			}
+		});
+		exactitudResultado.add(rdbtnMaloER);
+		rdbtnMaloER.setBounds(359, 364, 87, 23);
+		contentPane.add(rdbtnMaloER);
+		
+
+		
+	}
+	
+	public void establecerDefault() {
+		algoritmo.setSeguridadDeAcceso(EXCELENTE);
+		algoritmo.setExactitudDeResultados(EXCELENTE);
+	}
+}
