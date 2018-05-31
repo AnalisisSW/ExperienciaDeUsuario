f70488c3f044ec9faf89a353cd65e7ddccb083a9
Thomas <thomi.reynoso@hotmail.com>
2018-05-31 01:29:47 -0300

    Se termina interfaz de Usabilidad
    Falta el codigo

--- src/swing/Usabilidad.java
+++ src/swing/Usabilidad.java
@@ -9,6 +9,11 @@ import javax.swing.border.EmptyBorder;
 
 import main.AlgoritmoDeCalidad;
 import sun.security.x509.AlgorithmId;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextPane;
+import javax.swing.JRadioButton;
+import javax.swing.JButton;
 
 public class Usabilidad extends JFrame {
 
@@ -35,13 +40,111 @@ public class Usabilidad extends JFrame {
 	 * Create the frame.
 	 */
 	public Usabilidad(AlgoritmoDeCalidad algoritmo) {
+		setTitle("Usabilidad");
 		this.alg = algoritmo;
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 631, 571);
+		setBounds(100, 100, 602, 643);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
+		
+		JLabel lblCapacidadDeSer = new JLabel("Capacidad de ser Entendido:");
+		lblCapacidadDeSer.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblCapacidadDeSer.setBounds(24, 13, 221, 29);
+		contentPane.add(lblCapacidadDeSer);
+		
+		JTextPane txtpnCapacidadDelProducto = new JTextPane();
+		txtpnCapacidadDelProducto.setText("Capacidad del producto software para ayudar a los usuarios cuando necesiten asistencia.\r\n\r\nCaracter\u00EDsticas a medir: \r\n\u2022Posee ayuda contextual sobre men\u00FAes y botones de acci\u00F3n\r\n\u2022Incorporaci\u00F3n de manual de usuario en el sistema con un men\u00FA dedicado.");
+		txtpnCapacidadDelProducto.setBounds(24, 55, 299, 134);
+		contentPane.add(txtpnCapacidadDelProducto);
+		
+		JRadioButton rdbtnExcelenteCE = new JRadioButton("Excelente");
+		rdbtnExcelenteCE.setBounds(351, 35, 127, 25);
+		contentPane.add(rdbtnExcelenteCE);
+		
+		JRadioButton rdbtnMuyBuenoCE = new JRadioButton("Muy Bueno");
+		rdbtnMuyBuenoCE.setBounds(351, 65, 127, 25);
+		contentPane.add(rdbtnMuyBuenoCE);
+		
+		JRadioButton rdbtnBuenoCE = new JRadioButton("Bueno");
+		rdbtnBuenoCE.setBounds(351, 95, 127, 25);
+		contentPane.add(rdbtnBuenoCE);
+		
+		JRadioButton rdbtnRegularCE = new JRadioButton("Regular");
+		rdbtnRegularCE.setBounds(351, 125, 127, 25);
+		contentPane.add(rdbtnRegularCE);
+		
+		JRadioButton rdbtnMaloCE = new JRadioButton("Malo");
+		rdbtnMaloCE.setBounds(351, 155, 127, 25);
+		contentPane.add(rdbtnMaloCE);
+		
+		JLabel lblCapacidadDeSer_1 = new JLabel("Capacidad de ser Operado:");
+		lblCapacidadDeSer_1.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblCapacidadDeSer_1.setBounds(24, 202, 221, 29);
+		contentPane.add(lblCapacidadDeSer_1);
+		
+		JTextPane txtpnApacidadDelProducto = new JTextPane();
+		txtpnApacidadDelProducto.setText("\u2022Capacidad del producto software para ayudar a los usuarios cuando necesiten asistencia.\r\n\r\n\u2022Manual de usuario incorporado al sistema como un men\u00FA dedicado");
+		txtpnApacidadDelProducto.setBounds(24, 244, 299, 134);
+		contentPane.add(txtpnApacidadDelProducto);
+		
+		JRadioButton rdbtnExcelenteCO = new JRadioButton("Excelente");
+		rdbtnExcelenteCO.setBounds(351, 237, 127, 25);
+		contentPane.add(rdbtnExcelenteCO);
+		
+		JRadioButton rdbtnMuyBuenoCO = new JRadioButton("Muy Bueno");
+		rdbtnMuyBuenoCO.setBounds(351, 263, 127, 25);
+		contentPane.add(rdbtnMuyBuenoCO);
+		
+		JRadioButton rdbtnBuenoCO = new JRadioButton("Bueno");
+		rdbtnBuenoCO.setBounds(351, 293, 127, 25);
+		contentPane.add(rdbtnBuenoCO);
+		
+		JRadioButton rdbtnRegularCO = new JRadioButton("Regular");
+		rdbtnRegularCO.setBounds(351, 323, 127, 25);
+		contentPane.add(rdbtnRegularCO);
+		
+		JRadioButton rdbtnMaloCO = new JRadioButton("Malo");
+		rdbtnMaloCO.setBounds(351, 353, 127, 25);
+		contentPane.add(rdbtnMaloCO);
+		
+		JLabel lblCapacidadDeSer_2 = new JLabel("Capacidad de ser Atractivo:");
+		lblCapacidadDeSer_2.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblCapacidadDeSer_2.setBounds(24, 403, 221, 29);
+		contentPane.add(lblCapacidadDeSer_2);
+		
+		JTextPane txtpnCapacidadDelProducto_1 = new JTextPane();
+		txtpnCapacidadDelProducto_1.setText("\u2022Capacidad del producto software de ser utilizado sin asistencia adicional.\r\n");
+		txtpnCapacidadDelProducto_1.setBounds(24, 445, 307, 54);
+		contentPane.add(txtpnCapacidadDelProducto_1);
+		
+		JRadioButton rdbtnExcelente = new JRadioButton("Excelente");
+		rdbtnExcelente.setBounds(351, 406, 127, 25);
+		contentPane.add(rdbtnExcelente);
+		
+		JRadioButton rdbtnMuyBueno = new JRadioButton("Muy Bueno");
+		rdbtnMuyBueno.setBounds(351, 434, 127, 25);
+		contentPane.add(rdbtnMuyBueno);
+		
+		JRadioButton rdbtnBueno = new JRadioButton("Bueno");
+		rdbtnBueno.setBounds(351, 464, 127, 25);
+		contentPane.add(rdbtnBueno);
+		
+		JRadioButton rdbtnRegular = new JRadioButton("Regular");
+		rdbtnRegular.setBounds(351, 494, 127, 25);
+		contentPane.add(rdbtnRegular);
+		
+		JRadioButton rdbtnMalo = new JRadioButton("Malo");
+		rdbtnMalo.setBounds(351, 524, 127, 25);
+		contentPane.add(rdbtnMalo);
+		
+		JButton btnSalir = new JButton("Salir");
+		btnSalir.setBounds(24, 558, 97, 25);
+		contentPane.add(btnSalir);
+		
+		JButton btnFinalizar = new JButton("Siguiente");
+		btnFinalizar.setBounds(445, 558, 97, 25);
+		contentPane.add(btnFinalizar);
 	}
-
 }
